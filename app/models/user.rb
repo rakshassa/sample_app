b3e2d12cd614e9cfb0bd883c4149b91cdb4967cb
Elijah Hall <rakshassa@gmail.com>
2014-02-17 13:41:48 -0600

    Create user model with secure passwords

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ActiveRecord::Base
+
+	before_save { self.email = email.downcase }
+
+	validates(:name,  {presence: true, length: { maximum: 50 } } )
+	
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	validates :email, presence: true, 
+  				format:     { with: VALID_EMAIL_REGEX }, 
+  				uniqueness: { case_sensitive: false }
+	
+	has_secure_password
+	validates :password, length: { minimum: 6 }
+
+      				
+
+	# can also be written as 
+	# validates :name, presence: true
+	# because parens for a func are optional, and a final param of type hash can optionally use curly braces
+
+end
